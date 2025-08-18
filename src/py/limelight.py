cdd01837951af85d429246bf172061fb9dfbf462
dolphinpod <thomas.fasan2@gmail.com>
2025-08-18 03:36:19 -0400

    working on limelight script
    memory leak.. it will be much safer to run the solver in its own process instead of the limelight snapscript stuff

--- src/py/limelight.py
+++ src/py/limelight.py
@@ -22,67 +22,77 @@ class Solver:
         )
 
     def __del__(self):
-        if hasattr(self, "_handle") and self._handle:
-            self.lib.solver_destroy(self._handle)
+        self.lib.solver_destroy(self._handle)
 
 def loadLibrary():
-    return cdll.LoadLibrary('/home/dolphinpod/libsolver.so')
+    return cdll.LoadLibrary('/home/sigmacorns/libsolver.so')
 
-addUser = False
+addUser = True
 logIP = False
 solver = None
 
 def runPipeline(image, llrobot):
+    #return np.array([[]]), image, [0, 0, 0, 0, 0, 0, 0, 0]
+
     global addUser
-    global logIP
-    global lib
     global solver
-    if addUser:
-       result = subprocess.run("echo \"pass\npass\" | sudo adduser sigma sudo", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-       print(str(result.stdout).replace('\\n', '\n').replace('\\t', '\t'))
-       print(result)
-       addUser = False
-    if logIP:
-       result = subprocess.run("ip addr", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-       print(str(result.stdout).replace('\\n', '\n').replace('\\t', '\t'))
-       print(result)
-       logIP = False
-
-    if solver is None:
-        lib = loadLibrary()
-        SolverHandle = c_void_p
-        lib.solver_create.restype = SolverHandle
-        lib.solver_destroy.argtypes = [SolverHandle]
-        lib.solver_solve.argtypes = [
-            SolverHandle,
-            POINTER(c_double),  # target
-            POINTER(c_double),  # x0
-            POINTER(c_double),  # u_last
-            POINTER(c_double),  # p
-            POINTER(c_double),  # x_out
-            POINTER(c_double)   # u_out
-        ]
-        lib.solver_solve.restype = c_int
-        solver = Solver(lib)
+    global logIP
+    try:
+        if addUser:
+            print("adding user")
+            result = subprocess.run("echo \"22377\n22377\n\" | sudo adduser sigmacorns", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            print(str(result.stdout).replace('\\n', '\n').replace('\\t', '\t'))
+            print(result)
+            addUser = False
+        if logIP:
+            result = subprocess.run("ip addr", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            print(str(result.stdout).replace('\\n', '\n').replace('\\t', '\t'))
+            print(result)
+            logIP = False
     
-    n = 7  # example sizes
-    nu = 4
-    nx = 6
-    target = np.array([0.0,0.0,0.0,1.0,0.0,0.0,0.0,100.0], dtype=np.float64)
-    x0 = np.zeros(nx, dtype=np.float64)
-    u_last = np.zeros(nu, dtype=np.float64)
-    p = np.array([96.66,7.8,0.27,0.27,0.1016,68.0389,6.0], dtype=np.float64)
-    x_out = np.zeros((n+1)*nx, dtype=np.float64)
-    u_out = np.zeros(n*nu, dtype=np.float64)
+        if solver is None:
+            print("loading solver")
+            lib = loadLibrary()
+            SolverHandle = c_void_p
+            lib.solver_create.restype = SolverHandle
+            lib.solver_destroy.argtypes = [SolverHandle]
+            lib.solver_solve.argtypes = [
+                SolverHandle,
+                POINTER(c_double),  # target
+                POINTER(c_double),  # x0
+                POINTER(c_double),  # u_last
+                POINTER(c_double),  # p
+                POINTER(c_double),  # x_out
+                POINTER(c_double)   # u_out
+            ]
+            lib.solver_solve.restype = c_int
+            solver = Solver(lib)
+        
+        n = 7  # example sizes
+        nu = 4
+        nx = 6
+        target = np.array([0.0,0.0,0.0,1.0,0.0,0.0,0.0,100.0], dtype=np.float64)
+        x0 = np.zeros(nx, dtype=np.float64)
+        u_last = np.zeros(nu, dtype=np.float64)
+        p = np.array([96.66,7.8,0.27,0.27,0.1016,68.0389,6.0], dtype=np.float64)
+        x_out = np.zeros((n+1)*nx, dtype=np.float64)
+        u_out = np.zeros(n*nu, dtype=np.float64)
 
-    t0 = time.perf_counter()
+        t0 = time.perf_counter()
 
-    solver.solve(target,x0,u_last,p,x_out,u_out)
+        solver.solve(target,x0,u_last,p,x_out,u_out)
 
-    t1 = time.perf_counter()
+        t1 = time.perf_counter()
 
-    print("solved in " + str(t1-t0) + "s")
+        print("solved in " + str(t1-t0) + "s")
 
-    print("u = " + str(u_out))
+        time.sleep(0.5)
+
+        #print("u = " + str(u_out))
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        del solver
+    else:
+        del solver
     
     return np.array([[]]), image, [0, 0, 0, 0, 0, 0, 0, 0]
